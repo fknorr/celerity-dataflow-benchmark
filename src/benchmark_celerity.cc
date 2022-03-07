ab82b784b5f55d6db3e1f52afce33b0a7972689b
Fabian Knorr <fabian.knorr@uibk.ac.at>
2022-03-07 09:14:16 +0100

    Benchmark kernels with different dependencies

--- src/benchmark_celerity.cc
+++ src/benchmark_celerity.cc
@@ -1,19 +1,76 @@
 #include <celerity/celerity.h>
 #include <chrono>
 
-[[gnu::noinline]] void submit_and_wait(celerity::distr_queue& q) {
-	q.slow_full_sync();
+
+const celerity::range<1> global_range = 10000;
+constexpr int samples = 1000;
+
+
+[[gnu::noinline]] void sync_only(celerity::distr_queue &q, const celerity::buffer<float> &,
+                                 const celerity::experimental::host_object<void> &) {
+    q.slow_full_sync();
 }
 
-constexpr int N = 1000;
+[[gnu::noinline]] void kernels_without_dependencies(celerity::distr_queue &q, const celerity::buffer<float> &,
+                                                    const celerity::experimental::host_object<void> &) {
+    q.submit([](celerity::handler &cgh) {
+        cgh.parallel_for(global_range, [](celerity::item<1>) {});
+    });
+    q.submit([](celerity::handler &cgh) {
+        cgh.host_task(global_range, [](celerity::partition<1>) {});
+    });
+    q.slow_full_sync();
+}
+
+[[gnu::noinline]] void kernels_with_buffer_dependencies(celerity::distr_queue &q, const celerity::buffer<float> &buffer,
+                                                        const celerity::experimental::host_object<void> &) {
+    q.submit([=](celerity::handler &cgh) {
+        celerity::accessor acc{buffer, cgh, celerity::access::one_to_one{}, celerity::write_only, celerity::no_init};
+        cgh.parallel_for(global_range, [=](celerity::item<1> it) { acc[it] = M_PIf; });
+    });
+    q.submit([=](celerity::handler &cgh) {
+        celerity::accessor acc{buffer, cgh, celerity::access::one_to_one{}, celerity::read_write_host_task};
+        cgh.host_task(global_range, [=](celerity::partition<1> it) {
+            for (size_t i = 0; i < it.get_subrange().range[0]; ++i) {
+                acc[it.get_subrange().offset.get(0) + i] *= 2;
+            }
+        });
+    });
+    q.slow_full_sync();
+}
+
+[[gnu::noinline]] void kernels_with_host_object_dependencies(celerity::distr_queue &q, const celerity::buffer<float> &,
+                                                             const celerity::experimental::host_object<void> &obj) {
+    q.submit([=](celerity::handler &cgh) {
+        celerity::experimental::side_effect eff{obj, cgh};
+        cgh.host_task(global_range, [](celerity::partition<1> it) {});
+    });
+    q.submit([=](celerity::handler &cgh) {
+        celerity::experimental::side_effect eff{obj, cgh};
+        cgh.host_task(global_range, [](celerity::partition<1> it) {});
+    });
+    q.slow_full_sync();
+}
+
+template<typename Submit>
+void benchmark(const char *name, Submit submit, celerity::distr_queue &q, const celerity::buffer<float> &buffer,
+               const celerity::experimental::host_object<void> &obj) {
+    const auto start = std::chrono::system_clock::now();
+    for (int i = 0; i < samples; ++i) {
+        submit(q, buffer, obj);
+    }
+    const auto end = std::chrono::system_clock::now();
+    const auto time_per_iteration = (end - start) / samples;
+    CELERITY_INFO("{}: {} us / iter", name,
+                  std::chrono::duration_cast<std::chrono::duration<double, std::micro>>(time_per_iteration).count());
+}
 
 int main() {
-	celerity::distr_queue q;
-	const auto start = std::chrono::system_clock::now();
-	for(int i = 0; i < N; ++i) {
-		submit_and_wait(q);
-	}
-	const auto end = std::chrono::system_clock::now();
-	const auto time_per_iteration = (end - start) / N;
-	CELERITY_INFO("{} us / iter", std::chrono::duration_cast<std::chrono::duration<double, std::micro>>(time_per_iteration).count());
+    celerity::distr_queue q;
+    celerity::buffer<float> buffer{global_range};
+    celerity::experimental::host_object obj;
+    benchmark("sync only", sync_only, q, buffer, obj);
+    benchmark("kernels without dependencies", kernels_without_dependencies, q, buffer, obj);
+    benchmark("kernels with buffer dependencies", kernels_with_buffer_dependencies, q, buffer, obj);
+    benchmark("kernels with host object dependencies", kernels_with_host_object_dependencies, q, buffer, obj);
 }
--- src/benchmark_sycl.cc
+++ src/benchmark_sycl.cc
@@ -1,20 +1,56 @@
+#include <celerity/celerity.h>
 #include <chrono>
-#include <sycl/sycl.hpp>
 
-[[gnu::noinline]] void submit_and_wait(sycl::queue& q) {
-	auto evt = q.submit([](sycl::handler& cgh) { cgh.single_task([] {}); });
-	evt.wait();
+
+const sycl::range<1> global_range = 10000;
+constexpr int samples = 1000;
+
+
+[[gnu::noinline]] void sync_only(sycl::queue &q, sycl::buffer<float> &) {
+    q.wait();
 }
 
-constexpr int N = 1000;
+[[gnu::noinline]] void kernels_without_dependencies(sycl::queue &q, sycl::buffer<float> &) {
+    q.submit([](sycl::handler &cgh) {
+        cgh.parallel_for(global_range, [](sycl::item<1>) {});
+    });
+    q.submit([](sycl::handler &cgh) {
+        cgh.parallel_for(global_range, [](sycl::item<1>) {});
+    });
+    q.wait();
+}
+
+[[gnu::noinline]] void kernels_with_buffer_dependencies(sycl::queue &q, sycl::buffer<float> &buffer) {
+    q.submit([&](sycl::handler &cgh) {
+        sycl::accessor acc{buffer, cgh, sycl::write_only, sycl::no_init};
+        cgh.parallel_for(global_range, [=](sycl::item<1> it) { acc[it] = M_PIf; });
+    });
+    q.submit([&](sycl::handler &cgh) {
+        sycl::accessor acc{buffer, cgh, sycl::read_write};
+        cgh.parallel_for(global_range, [=](sycl::item<1> it) { acc[it] *= 2; });
+    });
+    q.wait();
+}
+
+template<typename Submit>
+void benchmark(const char *name, Submit submit, sycl::queue &q, sycl::buffer<float> &buffer) {
+    const auto start = std::chrono::system_clock::now();
+    for (int i = 0; i < samples; ++i) {
+        submit(q, buffer);
+    }
+    const auto end = std::chrono::system_clock::now();
+    const auto time_per_iteration = (end - start) / samples;
+    fmt::print(stderr, "{}: {} us / iter\n", name,
+               std::chrono::duration_cast<std::chrono::duration<double, std::micro>>(time_per_iteration).count());
+}
 
 int main() {
-	sycl::queue q;
-	const auto start = std::chrono::system_clock::now();
-	for(int i = 0; i < N; ++i) {
-		submit_and_wait(q);
-	}
-	const auto end = std::chrono::system_clock::now();
-	const auto time_per_iteration = (end - start) / N;
-	std::clog << std::chrono::duration_cast<std::chrono::duration<double, std::micro>>(time_per_iteration).count() << " us / iter\n";
+    // Re-use celerity's unified device selection
+    celerity::runtime::init(nullptr, nullptr);
+    sycl::queue &q = celerity::detail::runtime::get_instance().get_device_queue().get_sycl_queue();
+
+    sycl::buffer<float> buffer{global_range};
+    benchmark("sync only", sync_only, q, buffer);
+    benchmark("kernels without dependencies", kernels_without_dependencies, q, buffer);
+    benchmark("kernels with buffer dependencies", kernels_with_buffer_dependencies, q, buffer);
 }
